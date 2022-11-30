9fd60bf241776615f03ed8c752677648584a3b29
ohagi14 <furikake.hs@gmail.com>
2022-11-30 10:59:50 +0900

    ♻️ ヘッドライン部分をコンポーネント化

---
+++ components/Headline.tsx
@@ -0,0 +1,14 @@
+import styles from "../styles/Home.module.css";
+
+export function Headline(props:any) {
+  return (
+    <div className="Headline">
+      <h1 className={styles.title}>{props.page} page</h1>
+
+      <p className={styles.description}>
+        Get started by editing{" "}
+        <code className={styles.code}>pages/{props.page}.tsx</code>
+      </p>
+    </div>
+  );
+}
